5d62bdeac5bd7d5d824fad5802ff7f9f59bf8f1e
camshaft <bytheway.cameron@gmail.com>
2015-11-02 16:05:16 -0700

    release 0.1.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule PlugXForwardedFor.Mixfile do
 
   def project do
     [app: :plug_x_forwarded_for,
-     version: "0.0.1",
+     version: "0.1.0",
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
